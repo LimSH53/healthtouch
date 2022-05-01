5c58c1a07cdf89c7cf4acc328b10d27730d7a6e6
kimtg <taegyoung98@gmail.com>
2022-05-01 23:20:23 +0900

    notice 화면 DTO추가

---
+++ src/main/java/com/jaspa/healthtouch/notice/model/dto/NoticeDTO.java
@@ -0,0 +1,19 @@
+package com.jaspa.healthtouch.notice.model.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class NoticeDTO {
+	private int noticeNo;
+	private String memberId;
+	private String noticeTitle;
+	private String noticeContent;
+	private String noticeDelete;
+	private Date noticeDate;
+	private Date noticeEditDate;
+	private int noticeView;
+
+}
+ 
