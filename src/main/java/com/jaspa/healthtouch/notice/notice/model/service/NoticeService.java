d5a310604f47a62099d728265b545f6f09c5e12f
Cota <taegyoung98@gmail.com>
2022-05-07 05:58:20 +0900

    .

--- src/main/java/com/jaspa/healthtouch/notice/notice/model/service/NoticeService.java
+++ src/main/java/com/jaspa/healthtouch/notice/notice/model/service/NoticeService.java
@@ -2,13 +2,27 @@ package com.jaspa.healthtouch.notice.notice.model.service;
 
 
 
+
+import java.util.List;
+
 import com.jaspa.healthtouch.notice.notice.model.dto.NoticeDTO;
 
 
 public interface NoticeService {
 		
+		//공지사항 조회
+		List<NoticeDTO> noticeList() throws Exception ;
 
+		
+		//공지사항 등록
 		void registNotice(NoticeDTO notice) throws Exception;
+
+
+		NoticeDTO selectNoticeDetail(int noticeNo) throws Exception ;
+
+
+
+	
 	
 }
  
--- src/main/java/com/jaspa/healthtouch/notice/notice/model/service/NoticeServiceImpl.java
+++ src/main/java/com/jaspa/healthtouch/notice/notice/model/service/NoticeServiceImpl.java
@@ -2,8 +2,11 @@ package com.jaspa.healthtouch.notice.notice.model.service;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 import com.jaspa.healthtouch.notice.notice.model.dao.NoticeMapper;
 import com.jaspa.healthtouch.notice.notice.model.dto.NoticeDTO;
@@ -12,17 +15,37 @@ import com.jaspa.healthtouch.notice.notice.model.dto.NoticeDTO;
 @Service("noticeService")
 public class NoticeServiceImpl implements NoticeService{
 	
+	
 	@Autowired
 	public NoticeMapper noticeMapper;
 	
-
+		
+	//공지사항 조회
+	  @Override
+		public List<NoticeDTO> noticeList() throws Exception {
+		        return noticeMapper.noticeList();
+		    }
+	  
+	
+	//공지사항 등록
 	@Override
 	public void registNotice(NoticeDTO notice)throws Exception  {
 		noticeMapper.registNotice(notice);
 		
- 
+
 		}
-	}
+
+	
+	  
+	 public NoticeDTO selectNoticeDetail(int noticeNo) throws Exception {
+		 noticeMapper.viewCount(noticeNo);
+			
+		 NoticeDTO notice = noticeMapper.selectNoticeDetail(noticeNo);
+			
+			return notice;
+	 }
+	  
+}
 	
  
 	 
