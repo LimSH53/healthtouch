dbad944b2473040a79b7ad76f7ea260d42c21334
tvovt0306 <tvovt0306@gmail.com>
2022-05-14 03:00:19 +0900

    locker 기능미완성

---
+++ src/main/java/com/jaspa/healthtouch/member/locker/model/dao/LockerMapper.java
@@ -0,0 +1,19 @@
+package com.jaspa.healthtouch.member.locker.model.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.jaspa.healthtouch.member.locker.model.dto.LockerDTO;
+
+
+@Mapper
+public interface LockerMapper {
+	
+	/* 라커 전체조회 */
+	List<LockerDTO> selectAllLocker();
+	
+	/* 라커 등록 */
+	int updateLocker(LockerDTO locker);
+	
+}
