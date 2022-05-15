ca84f93b4caf99c4b6be792de2cc792cecd7b085
tvovt0306 <tvovt0306@gmail.com>
2022-05-15 20:34:17 +0900

    locker css commit

--- src/main/java/com/jaspa/healthtouch/member/locker/controller/LockerMemberController.java
+++ src/main/java/com/jaspa/healthtouch/member/locker/controller/LockerMemberController.java
@@ -3,13 +3,18 @@ package com.jaspa.healthtouch.member.locker.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.jaspa.healthtouch.login.model.dto.UserImpl;
 import com.jaspa.healthtouch.member.locker.model.dto.LockerDTO;
 import com.jaspa.healthtouch.member.locker.model.service.LockerMemberService;
+import com.jaspa.healthtouch.trainer.trainerInfo.model.dto.TrainerMemberDTO;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -41,6 +46,41 @@ public class LockerMemberController {
 		
 		return mv;
 	}
+	
+	
+	
+	
+	
+	
+	
+	@GetMapping("/updateLocker")
+	public void updateLocker(Model model, @AuthenticationPrincipal UserImpl user) {
+		
+		LockerDTO lockerInfo = lockerMemberService.updateLocker(user.getId());
+		model.addAttribute("lockerInfo", lockerInfo);
+		
+		log.info(lockerInfo.toString());
+	}
+	
+	
+	@PostMapping("/updateLocker")
+	public void updateLocker(TrainerMemberDTO trainer,  @AuthenticationPrincipal UserImpl user, Model model) {
+		
+		trainer.setId(user.getId());
+		log.info(trainer.toString());
+		trainerService.updateTrainer(trainer);
+		
+		TrainerMemberDTO trainerInfo = trainerService.selectTrainer(user.getId());
+		model.addAttribute("trainerInfo", trainerInfo);
+		
+		user.setName(trainer.getName());
+		user.setContact(trainer.getContact());
+		user.setAddress(trainer.getAddress());
+		user.setBirthday(trainer.getBirthday());
+	}
+	
+	
+	
 	
 	
 	
