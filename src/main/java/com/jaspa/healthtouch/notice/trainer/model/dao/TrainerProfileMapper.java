88a70cefaef68da57a6675d96acde91ecdaa0e25
Cota <taegyoung98@gmail.com>
2022-05-18 14:38:22 +0900

    트레이너 조회 부분 재 머지

--- src/main/java/com/jaspa/healthtouch/notice/trainer/model/dao/TrainerProfileMapper.java
+++ src/main/java/com/jaspa/healthtouch/notice/trainer/model/dao/TrainerProfileMapper.java
@@ -2,6 +2,7 @@ package com.jaspa.healthtouch.notice.trainer.model.dao;
 
 
 
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.jaspa.healthtouch.member.product.model.dto.ReviewDTO;
@@ -10,15 +11,7 @@ public interface TrainerProfileMapper {
 
 
 	//트레이너 후기 조회(서현승)
-	ReviewDTO trainerProfile(int no, String memberId);
+	ReviewDTO trainerprofile();
 
-	//트레이너 후기 조회(남지현)
-	ReviewDTO trainerProfile1(int no, String memberId);
 
-	//트레이너 후기 조회(허승준)
-	ReviewDTO trainerProfile2(int no, String memberId);
-
-	//트레이너 후기 조회(한주은)
-	ReviewDTO trainerProfile3(int no, String memberId);
-	
 }
