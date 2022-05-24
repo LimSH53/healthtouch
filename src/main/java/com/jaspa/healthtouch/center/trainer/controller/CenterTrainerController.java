9842411bb9a4076a4d04d48a0dbd28fe7304390d
최서경 <sg22049@gmail.com>
2022-05-24 10:49:19 +0900

    휴가 캘린더 수정

--- src/main/java/com/jaspa/healthtouch/center/trainer/controller/CenterTrainerController.java
+++ src/main/java/com/jaspa/healthtouch/center/trainer/controller/CenterTrainerController.java
@@ -1,23 +1,17 @@
 package com.jaspa.healthtouch.center.trainer.controller;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.maven.lifecycle.Schedule;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -27,6 +21,7 @@ import com.jaspa.healthtouch.center.trainer.model.dto.HolidayDTO;
 import com.jaspa.healthtouch.center.trainer.model.dto.TrainerInfoDTO;
 import com.jaspa.healthtouch.center.trainer.model.dto.TrainerSalaryDTO;
 import com.jaspa.healthtouch.center.trainer.model.service.TrManagementService;
+import com.jaspa.healthtouch.common.model.dto.FullCalendarDTO;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -108,26 +103,70 @@ public class CenterTrainerController {
 	}
 	
 	//휴가 캘린더
+//	@GetMapping("/hday/calendar")
+//    @ResponseBody
+//    public List<Map<String, Object>> getCalecdar() {
+//        List<HolidayDTO> holidayList = trManagementService.findAllHoliday();
+// 
+//        JSONObject jsonObj = new JSONObject();
+//        JSONArray jsonArr = new JSONArray();
+// 
+//        HashMap<String, Object> hash = new HashMap<>();
+// 
+//        for (int i = 0; i < holidayList.size(); i++) {
+//            hash.put("name", holidayList.get(i).getTrainer().getMember().getName());
+//            hash.put("date", holidayList.get(i).getHDay());
+//
+//            jsonObj = new JSONObject(hash);
+//            jsonArr.add(jsonObj);
+//        }
+//        log.info("jsonArrCheck: {}", jsonArr);
+//        return jsonArr;
+//    }
+//	
+//	@PostMapping("/hday/calendar")
+//    @ResponseBody
+//    public List<Map<String, Object>> monthPlan() {
+//        List<HolidayDTO> holidayList = trManagementService.findAllHoliday();
+// 
+//        JSONObject jsonObj = new JSONObject();
+//        JSONArray jsonArr = new JSONArray();
+// 
+//        HashMap<String, Object> hash = new HashMap<>();
+// 
+//        for (int i = 0; i < holidayList.size(); i++) {
+//            hash.put("name", holidayList.get(i).getTrainer().getMember().getName());
+//            hash.put("date", holidayList.get(i).getHDay());
+//
+//            jsonObj = new JSONObject(hash);
+//            jsonArr.add(jsonObj);
+//        }
+//        log.info("jsonArrCheck: {}", jsonArr);
+//        return jsonArr;
+//    }
+	@PostMapping("/hday/calendar")
+	@ResponseBody
+	public Map<String, FullCalendarDTO> holidayCalen() {
+		Map<String, FullCalendarDTO> hdayMap = new HashMap<>();
+		List<HolidayDTO> holidayList = trManagementService.findAllHoliday();
+		
+		for(int i = 0; i < holidayList.size(); i++) {
+			HolidayDTO holiday = holidayList.get(i);
+			String title = holiday.getTrainer().getMember().getName();
+			Date hDay = holiday.getHDay();
+			
+			hdayMap.put("event" + i, new FullCalendarDTO(title, hDay, true ));
+		}
+		
+		return hdayMap;
+	}
+	
+	//휴가 캘린터
 	@GetMapping("/hday/calendar")
-    @ResponseBody
-    public List<Map<String, Object>> monthPlan() {
-        List<HolidayDTO> holidayList = trManagementService.findAllHoliday();
- 
-        JSONObject jsonObj = new JSONObject();
-        JSONArray jsonArr = new JSONArray();
- 
-        HashMap<String, Object> hash = new HashMap<>();
- 
-        for (int i = 0; i < holidayList.size(); i++) {
-            hash.put("name", holidayList.get(i).getTrainer().getMember().getName());
-            hash.put("date", holidayList.get(i).getHDay());
-
-            jsonObj = new JSONObject(hash);
-            jsonArr.add(jsonObj);
-        }
-        log.info("jsonArrCheck: {}", jsonArr);
-        return jsonArr;
-    }
+	public void holidayCalendar() {}
+	
+	
+	
 
 	
 	// 근태 조회
--- src/main/resources/static/js/center/holiday.js
+++ src/main/resources/static/js/center/holiday.js
@@ -1,71 +1,36 @@
-/*$('#holidayCalendar').on('click', function() {
-	        var calendarEl = document.getElementById('calendar');
-	        var calendar = new FullCalendar.Calendar(calendarEl, {
-	          initialView: 'dayGridMonth',
-	          locale: 'ko',
-			  themeSystem: 'bootstrap5'
-	        });
-	        calendar.render();
-	      });*/
-
-
+//휴가 캘린더
+document.getElementById("holidayCalendar").addEventListener("click", viewCalendar);
  
-document.addEventListener('DOMContentLoaded', function () {
-            $(function () {
-                var request = $.ajax({
-                    url: "/center/trainer/hday/calendar", // 변경하기
-                    method: "GET",
-                    dataType: "json"
-                });
- 
-                request.done(function (data) {
-                    console.log(data); // log 로 데이터 찍어주기.
- 
-                    var calendarEl = document.getElementById('calendar');
- 
-                    var calendar = new FullCalendar.Calendar(calendarEl, {
-                        initialView: 'dayGridMonth',
-	          			locale: 'ko',
-		  				themeSystem: 'bootstrap5',
-                        headerToolbar: {
-                            left: 'prev,next today',
-                            center: 'title',
-                            right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-                        },
-                        editable: true,
-                        droppable: true, // this allows things to be dropped onto the calendar
-                        drop: function (arg) {
-                            // is the "remove after drop" checkbox checked?
-                            if (document.getElementById('drop-remove').checked) {
-                                // if so, remove the element from the "Draggable Events" list
-                                arg.draggedEl.parentNode.removeChild(arg.draggedEl);
-                            }
-                        },
-                        /**
-                         * data 로 값이 넘어온다. log 값 전달.
-                         */
-                        events: data
-                    });
- 
-                    calendar.render();
-                });
- 
-                request.fail(function( jqXHR, textStatus ) {
-                    alert( "Request failed: " + textStatus );
-                });
-            });
- 
+function viewCalendar () {
+	var calendarEl = document.getElementById('calendar');
+		var calendar = new FullCalendar.Calendar(calendarEl, {
+			initialView: 'dayGridMonth',
+			locale: 'ko',
+			themeSystem: 'bootstrap5'
         });
-
-           
- 
+		
+		var arr = '';
+		
+		$.ajax({
+			dataType: 'json',
+			url: '/center/trainer/hday/calendar',
+			type: 'post',
+			async: false,
+			success: function(hdayMap) {
+				arr = hdayMap;
+			},
+			error: function(jqXHR, textStatus) {
+				 alert( "요청 실패 : " + textStatus );
+			}
+		})
+		
+		$.each(arr, function(index, item) {
+			calendar.addEvent(item);
+		});
+		
+		calendar.render();
+}
         
-
-
-
-     
-
-    
 
 /* 승인 버튼 클릭 시*/	
 $('.approval').on('click', function() {
